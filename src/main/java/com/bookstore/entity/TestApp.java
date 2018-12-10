3dfae7055bd0e6c7579dc6b902689c7a40ac57d2
truongphan0810 <vantruong0810@gmail.com>
2018-12-10 18:47:00 +0700

    Updated code

--- src/main/java/com/bookstore/entity/TestApp.java
+++ src/main/java/com/bookstore/entity/TestApp.java
@@ -1,22 +1,29 @@
 package com.bookstore.entity;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class TestApp {
-
+	
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jee_practice");
 		EntityManager em = emf.createEntityManager();
 		
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-		
-		tx.commit();
-		
+		em.getTransaction().begin();
+		@SuppressWarnings("unchecked")
+		List<BookEntity> Students = em.createQuery("from BookEntity").getResultList();
+		for (Iterator<BookEntity> iterator = Students.iterator(); iterator.hasNext();) {
+			BookEntity bookEntity = (BookEntity) iterator.next();
+	        System.out.println(bookEntity.getDescription());
+	      }
+		em.getTransaction().commit();
+
 		em.close();
 		emf.close();
 	}
